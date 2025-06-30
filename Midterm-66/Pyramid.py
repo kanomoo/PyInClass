0a1608693c18f311a010eb7975130d6ba11bd44e
kanomoo <few717254@gmail.com>
2025-06-30 21:39:49 +0700

    mid 66

---
+++ Midterm-66/Pyramid.py
@@ -0,0 +1,21 @@
+name = input("Enter string for Pyramid : ") # RukSomchai
+num = len(name)
+mess = ""
+for i in range(num):
+    mess += (" " * (num - i))
+    for n in name[:i+1]:
+        mess += f"{n} "
+    mess += "\n"
+print(mess)
+
+#show in terminal
+# name = "RukSomchai"
+# print(f"Enter string for Pyramid : {name}") # RukSomchai
+# num = len(name)
+# mess = ""
+# for i in range(num):
+#     mess += (" " * (num - i))
+#     for n in name[:i+1]:
+#         mess += f"{n} "
+#     mess += "\n"
+# print(mess)
