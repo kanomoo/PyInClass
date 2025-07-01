eec49be9b5be3bb47fdd5cbd971baae7494078b5
kanomoo <few717254@gmail.com>
2025-07-01 23:45:34 +0700

    mid

---
+++ Midterm67/number_palindrome.py
@@ -0,0 +1,18 @@
+# num = input("Enter number : ") #123456
+# re = num[::-1]
+# mess = ""
+# for i in range(len(num)):
+#     mess += " " * i + re[i:]
+#     mess += num[1:len(num) - i] + "\n"
+# print(mess)
+
+
+# ทำไว้ show ใน terminal
+num = "123456"
+print(f"Enter number : {num}") #123456
+re = num[::-1]
+mess = ""
+for i in range(len(num)):
+    mess += " " * i + re[i:]
+    mess += num[1:len(num) - i] + "\n"
+print(mess)
