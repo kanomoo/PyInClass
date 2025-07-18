f65e8d37cc30216736e91b02819739e0219938bb
Few <few717254@gmail.com>
2025-07-18 12:14:43 +0700

    t

---
+++ Midterm-66/test.py
@@ -0,0 +1,19 @@
+names, scores = [],[]
+print("Input Data:")
+for i in range(5):
+    name = input(f"Enter subject name({i+1}) : ")
+    score = int(input(f"Enter subject score({i+1}) : "))
+    g = {80:("A",4),70:("B",3),60:"C",50:"D",0:"F"}
+    for k,v in g.items():
+        if score >= k:
+            grade,point = v
+    print()
+    names.append(name)
+    scores.append(score)
+print("Grade Point Average(GPA) Report")
+head = "No. Subject Name        Mark  Grade    Points"
+line = "=" * (len(head) + 3)
+print(line,head,line,sep="\n")
+for i in range(5):
+    print(f"")
+
