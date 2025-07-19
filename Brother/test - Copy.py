7a1af3ae0d74d953aa799eb472ea8c69256b1c20
kanomoo <few717254@gmail.com>
2025-07-20 01:46:27 +0700

    t

--- Brother/test - Copy.py
+++ Brother/test - Copy.py
@@ -1,18 +1,35 @@
-c1 = int(input("Player 1 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: "))
-c2 = int(input("\nPlayer 2 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: "))
+c1 = input("Player 1 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: ")
+c2 = input("\nPlayer 2 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: ")
+g = {"1":"Rock","2":"Paper","3":"Scissors"}
+r1,r2 = g[c1] if c1 in g else "invalid", g[c2] if c2 in g else "invalid"
 
-g = {1:"Rock",2:"Paper",3:"Scissors"}
-for i in g:
-    if c1 == i: r1 = g[i]
-    if c2 == i: r2 = g[i]
-
-if c1 >3 and c2 >3 : result = "Invalid input! Please enter 1, 2, or 3 only."
-elif c1 == c2: result = "It's a tie!"
-elif c1 > c2: 
-    result = "Player 1 wins!"
-    if c2 == 1 and c1 == 3: result = "Player 2 wins!"
-elif c2 > c1: 
-    result = "Player 2 wins!"
-    if c1 == 1 and c2 == 3: result = "Player 1 wins!"
+try:
+    if c1 == "" or c2 == "": result  = "Invalid input! Please enter 1, 2, or 3 only."
+    elif int(c1) > 3 or int(c2) > 3: result = "Invalid input! Please enter 1, 2, or 3 only."
+    elif c1 == c2: result = "It's a tie!"
+    elif c1 > c2: result = "Player 2 wins!" if c2 == "1" and c1 == "3" else "Player 1 wins!"
+    elif c2 > c1: result = "Player 1 wins!" if c1 == "1" and c2 == "3" else "Player 2 wins!"
+except ValueError:pass
 
 print(f"\nPlayer 1 chose: {r1}\nPlayer 2 chose: {r2}\n{result}")
+
+
+
+# c1 = int(input("Player 1 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: "))
+# c2 = int(input("\nPlayer 2 - Choose your move:\n1 = Rock\n2 = Paper\n3 = Scissors\nEnter 1, 2, or 3: "))
+#
+# g = {1:"Rock",2:"Paper",3:"Scissors"}
+# for i in g:
+#     if c1 == i: r1 = g[i]
+#     if c2 == i: r2 = g[i]
+#
+# if c1 >3 and c2 >3 : result = "Invalid input! Please enter 1, 2, or 3 only."
+# elif c1 == c2: result = "It's a tie!"
+# elif c1 > c2:
+#     result = "Player 1 wins!"
+#     if c2 == 1 and c1 == 3: result = "Player 2 wins!"
+# elif c2 > c1:
+#     result = "Player 2 wins!"
+#     if c1 == 1 and c2 == 3 : result = "Player 1 wins!"
+#
+# print(f"\nPlayer 1 chose: {r1}\nPlayer 2 chose: {r2}\n{result}")
--- PythonCookbook/ts01.py
+++ PythonCookbook/ts01.py
@@ -1,4 +1,8 @@
+from fontTools.varLib.plot import stops
+from pandas.core.methods.selectn import SelectNSeries
+
 print("===========18===========".center(72))####################################################################
+
 from random import randint
 l_word = ["ศูนย์" ,"หนึ่ง", "สอง", "สาม", "สี่", "ห้า" ]
 r = randint(0,10)
@@ -32,7 +36,9 @@ print(*a,sep=", ")
 
 a = remove_list(l_word, " ") #.ใส่ try ไว้ไม่ว่ายังไงก็คืนค่า lst
 print(*a,sep=", ")
+
 print("===========19===========".center(72))####################################################################
+
 # x = -1
 # if x < 0: raise Exception("ข้อมูลเป็นจำนวนลบ") # raise เป็นการสั่งให้แสดงข้อผิดพลาด
 
@@ -49,7 +55,9 @@ except Exception as err: print(err)
 else: print("ข้อมูลเป็นจำนวนบวก")
 finally: print("ขอบคุณครับ")
 ########################################################################
+
 print("===========20===========".center(72))####################################################################
+
 try:
     # a = int(input("ใส่เลขจำนวนเต็มที่มากกว่า 0 >> "))
     a = -1
@@ -84,7 +92,9 @@ else: print(f"ค่าของจำนวนคือ : {value}")
 #     assert num <= 100 and num >= 1 , "ต้องกำหนดค่าระหหว่าง 1 - 100"
 # except Exception as err:print(err)
 # else: print(f"ค่าที่เหลือคือ: {num}")
+
 print("===========21===========".center(72))####################################################################
+
 a = 12
 if type(a) is int: print(True)
 if type(a).__name__ == "int": print(True) #.__name__ เปรียนเทียบโดยเขียนชนิดข้อมูลในแบบสตริง
@@ -93,3 +103,99 @@ print("########################################################################"
 a = 123
 b = -4.56
 c = True
+
+print("===========23===========".center(72))####################################################################
+
+a = eval("123") # eval แปลง string เป็นชนิดที่สอดคล้องกับค่ากี่กำหนด
+b = eval("[1,2,3]")
+print(type(a).__name__,type(b).__name__)
+print(a,b,sep="  ")
+
+print("===========24===========".center(72))####################################################################
+
+try:
+    # num = input("ใส่จำนวนเต็มหรือทศนิยม >> ")
+    num = "-3.14" # ต้องรับค่าเป็น string ก่อน
+    assert type(eval(num)) == float,"ต้องใส่เลขจำนวนหรือทศนิยมเท่านั้น" # eval แปลง string เป็นชนิดที่สอดคล้องกับค่ากี่กำหนด
+except Exception as err: print(err)
+else: print("ขอบคุณครับ")
+
+print("===========25===========".center(72))####################################################################
+
+x = 10
+# if x % 2 ==0: print("เลขคู่") แบบปกติ
+# else: print("เลขคี่")
+
+# การเขียน if else แบบบรรทัดเดียว
+y = 0
+z = x / y if y != 0 else print("ตัวหารเป็น 0 ไม่ได้")
+
+print("เลขคู่") if x % 2 == 0 else print("เลขคี่")
+print("เลขคู่" if x % 2 == 0 else "เลขคี่")
+
+s = "OK" if a > 0 else "Error"
+print(s)
+
+print("===========26===========".center(72))####################################################################
+
+import random
+balance = random.randint(1000, 10000)
+amount = random.randint(-100, 15000)
+balance -= amount if amount in range(1, balance) else 0
+print(balance)
+
+# ใช้รูปแบบ for-else และ while-else แบบมีเงื่อนไข
+sum = 0
+for i in range(11):
+    sum += i
+else: print(f"sum = {sum}")
+# แต่ถ้าเป็นแบบนี้ else จะไม่ทำงาน
+for i in range(11):
+    sum += i if sum != 6 else None
+print()
+# x = input("ใส่จำนวนเต็มบวกเท่านั้น >> ")
+# found_non_digit = False
+# i = 0
+# while i < len(x):
+#     if not x[i].isdigit():
+#         found_non_digit = True
+#         print("ใส่ค่าไม่ตรงเงื่อนไข")
+#         break
+#     i += 1
+# else: print("ใส่ค่าได้ถูกต้อง")
+
+# name = input("ใส่จำนวนเต็มบวกเท่านั้น >> ")
+name = "-1"
+print("ใส่ค่าได้ถูกต้อง" if name.isdigit() else "ใส่ค่าไม่ตรงเงื่อนไข")
+
+print("===========27===========".center(72))####################################################################
+
+# ใช้ match-case ทดแทน switch-case
+
+# size = input("enter size (s, m, l) >> ")
+size = "s"
+print(f"enter size (s, m, l) >> {size}")
+match size:
+    case "s": print("small")
+    case "m": print("medium")
+    case "l": print("large")
+    case _: print("error")
+
+# ถ้ามีหลายค่าที่จะดำเนินการแบบเดียวกัน
+# m = int(input("ลำดับของเดือนในรอบปี (1-12) >> "))
+m = 12
+print(f"ลำดับของเดือนในรอบปี (1-12) >> {m}")
+num_days = 0
+match m:
+    case 1 | 3 | 5 | 7| 8 | 10 | 12: num_days = 31
+    case 4 | 6 | 9 | 11: num_days = 30
+    case _: num_days = -1
+print(num_days)
+print("####################################################################")
+
+# ตรวจสอบไฟล์ที่รันด้วย if__name__ == "__main__" | __name__ จะเป็น __main__ ถ้าเรียกว่าไฟล์นี้ขึ้นมาทำงานโดยตรง
+print(__name__)
+import app
+app.run(),app.func()
+
+print("===========29===========".center(72))####################################################################
