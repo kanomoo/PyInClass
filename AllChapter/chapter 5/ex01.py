c2a22690c1f186ae9c3beb1151a3477efd397426
Your Name <you@example.com>
2025-07-22 13:44:04 +0700

    ex1

--- AllChapter/chapter 5/ex01.py
+++ AllChapter/chapter 5/ex01.py
@@ -2,6 +2,5 @@ print(">> Program Find Maximum Digit <<")
 num = None
 while num != 0:
     num = int(input("Enter integer number(0-exit) : "))
-    if num != 0:
-        print(f"Maximum Digit of interger number {num} = {max(str(num))}")
-    else:print("Exit Program")
+    if num != 0: print(f"Maximum Digit of interger number {num} = {max(str(num))}")
+    else: print("Exit Program")
