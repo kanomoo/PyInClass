af9f8655048121bd6a9530a7a05fb2d8e07b9c97
Few <few717254@gmail.com>
2025-07-27 20:54:15 +0700

    te

--- Midterm-67/number_palindrome.py
+++ Midterm-67/number_palindrome.py
@@ -17,7 +17,22 @@
 #     mess += num[1:len(num) - i] + "\n"
 # print(mess)
 
-# ทำไว้ show ใน terminal
-num = input("Enter : ")
-for i in range(len(num)):
-    print(" " * i,num[::-1][i:],num[1:(len(num)-i)],sep="")
+# # ทำไว้ show ใน terminal
+# num = input("Enter : ")
+# for i in range(len(num)):
+#     print(" " * i,num[::-1][i:],num[1:(len(num)-i)],sep="")
+
+num,re = input("Enter number : "),""
+re += str(int(num) % 10 // 1)
+re += str(int(num) % 100 // 10)
+re += str(int(num) % 1000 // 100)
+re += str(int(num) % 10000 // 1000)
+re += str(int(num) % 100000 // 10000)
+re += str(int(num) % 1000000 // 100000)
+print(re)
+
+# for i in range(1,len(num)+1):
+#     re += str(int(num) % (10*i) // (1*i))
+# print(re)
+
+
