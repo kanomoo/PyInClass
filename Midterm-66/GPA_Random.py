5a9936fa9514e1edfcf81aca610e312cf9435e9f
kanomoo <few717254@gmail.com>
2025-07-28 19:30:12 +0700

    randim

--- Midterm-66/GPA_Random.py
+++ Midterm-66/GPA_Random.py
@@ -87,17 +87,29 @@
 #             break
 
 from random import randint
-line,head = "=" * 26,format("Main Menu","^26")
-print(line,head,line," 1. Input number of subject (8)"," 2. start Random Grade"," 3. Exit Program",line,sep="\n")
-choice = int(input("Select your Choice : "))
-match choice:
-    case 1:
-        num = int(input("Enter your number of subject : "))
-        print("Press any key to continue . . .")
-        for i in range(num):
-            score = randint(0,100)
-            if 80 <= score <= 100: grade,point = "A",4.0
-            elif 70 >= score: grade,point = "B",3.0
-            elif 60 >= score: grade,point = "C",2.0
-            elif 50 >= score: grade,point = "D",1.0
-            else: grade,point = "F",0
+line,head,result,t_p,t_c = "=" * 26,format("Main Menu","^26"),"",0,0
+while True:
+    print(line,head,line," 1. Input number of subject (8)"," 2. Start Random Grade"," 3. Exit Program",line,sep="\n")
+    choice = int(input("Select your Choice : "))
+    match choice:
+        case 1:
+            num = int(input("Enter your number of subject : "))
+            for i in range(num):
+                score = randint(40,90)
+                if 80 <= score <= 100: grade,point = "A",4.0
+                elif score >= 70 : grade,point = "B",3.0
+                elif score >= 60: grade,point = "C",2.0
+                elif score >= 50: grade,point = "D",1.0
+                else: grade,point = "F",0
+                t_p += point
+                t_c += 3.0
+                result += f"Your #{i+1} subject is {grade}\n"
+            result += f"\nYour Total GPA is : {t_c / t_p :.2f}"
+            print("Press any key to continue . . .\n")
+        case 2:
+            print("Start Random Score&Grade . . . .",line,result,line,"Press any key to continue . . . ","",sep="\n")
+        case 3:
+            print("Exit Program . . .")
+            break
+        case _:
+            print("No choice try again . . .\n")
