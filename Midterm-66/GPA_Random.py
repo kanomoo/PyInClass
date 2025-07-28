99c297c87ebc2e1a7bfd737f07d372282dc2576d
Few <few717254@gmail.com>
2025-07-28 18:43:47 +0700

    ts

--- Midterm-66/GPA_Random.py
+++ Midterm-66/GPA_Random.py
@@ -1,87 +1,103 @@
+# from random import randint
+# total_p = 0
+# total_c = 0
+# while True:
+#     print("=" * 26)
+#     print(format("Main Menu","^26"))
+#     print("=" * 26)
+#     print(" 1. Input number of subject (8)")
+#     print(" 2. Start Random Grade")
+#     print(" 3. Exit Program")
+#     print("=" * 26)
+#     choice = input("Select your Choice : ")
+#     match choice:
+#         case "1":
+#             num = int(input("Enter your number of subject : "))
+#             print("Press any key to continue . . .\n")
+#         case "2":
+#             print("Start Random  Score&Grade . . . .")
+#             print("=" * 26)
+#             for i in range(num):
+#                 point = randint(40,90)
+#                 check_grade = {80:"A",70:"B",60:"C",50:"D",0:"F"}
+#                 for c in check_grade:
+#                     if point >= c:
+#                         grade = check_grade[c]
+#                         break
+#                 check_point = {"A":4,"B":3,"C":2,"D":1,"F":0}
+#                 for c in check_point:
+#                     if grade == c:
+#                         point = check_point[c] * 3
+#                 total_p += point
+#                 total_c += 3
+#                 print(f"Your #{i} subject is {grade}")
+#             print(f"\nYour Total GPA is : {total_p / total_c}")
+#             print("=" * 26)
+#             print("Press any key to continue . . .\n")
+#         case "3":
+#             print("Exit Program . . .")
+#             break
+#         case _:
+#             print("No choice")
+
+
+# # ทำไว้ show ใน terminal
+# from random import randint
+# total_p = 0
+# total_c = 0
+# choice = 0 #
+# while True:
+#     print("=" * 26)
+#     print(format("Main Menu","^26"))
+#     print("=" * 26)
+#     print(" 1. Input number of subject (8)")
+#     print(" 2. Start Random Grade")
+#     print(" 3. Exit Program")
+#     print("=" * 26)
+#     choice += 1 #
+#     print(f"Select your Choice : {choice}") #
+#     match str(choice):
+#         case "1":
+#             num = 5
+#             print(f"Enter your number of subject : {num}")
+#             print("Press any key to continue . . .\n")
+#         case "2":
+#             print("Start Random  Score&Grade . . . .")
+#             print("=" * 26)
+#             for i in range(num):
+#                 point = randint(40,90)
+#                 check_grade = {80:"A",70:"B",60:"C",50:"D",0:"F"}
+#                 for c in check_grade:
+#                     if point >= c:
+#                         grade = check_grade[c]
+#                         break
+#                 check_point = {"A":4,"B":3,"C":2,"D":1,"F":0}
+#                 for c in check_point:
+#                     if grade == c:
+#                         point = check_point[c] * 3
+#                 total_p += point
+#                 total_c += 3
+#
+#                 print(f"Your #{i+1} subject is {grade}")
+#             print(f"\nYour Total GPA is : {total_p / total_c}")
+#             print("=" * 26)
+#             print("Press any key to continue . . .\n")
+#         case "3":
+#             print("Exit Program . . .")
+#             break
+
 from random import randint
-total_p = 0
-total_c = 0
-while True:
-    print("=" * 26)
-    print(format("Main Menu","^26"))
-    print("=" * 26)
-    print(" 1. Input number of subject (8)")
-    print(" 2. Start Random Grade")
-    print(" 3. Exit Program")
-    print("=" * 26)
-    choice = input("Select your Choice : ")
-    match choice:
-        case "1":
-            num = int(input("Enter your number of subject : "))
-            print("Press any key to continue . . .\n")
-        case "2":
-            print("Start Random  Score&Grade . . . .")
-            print("=" * 26)
-            for i in range(num):
-                point = randint(40,90)
-                check_grade = {80:"A",70:"B",60:"C",50:"D",0:"F"}
-                for c in check_grade:
-                    if point >= c:
-                        grade = check_grade[c]
-                        break
-                check_point = {"A":4,"B":3,"C":2,"D":1,"F":0}
-                for c in check_point:
-                    if grade == c:
-                        point = check_point[c] * 3
-                total_p += point
-                total_c += 3
-                print(f"Your #{i} subject is {grade}")
-            print(f"\nYour Total GPA is : {total_p / total_c}")
-            print("=" * 26)
-            print("Press any key to continue . . .\n")
-        case "3":
-            print("Exit Program . . .")
-            break
-        case _:
-            print("No choice")
-
-
-# ทำไว้ show ใน terminal
-from random import randint
-total_p = 0
-total_c = 0
-choice = 0 #
-while True:
-    print("=" * 26)
-    print(format("Main Menu","^26"))
-    print("=" * 26)
-    print(" 1. Input number of subject (8)")
-    print(" 2. Start Random Grade")
-    print(" 3. Exit Program")
-    print("=" * 26)
-    choice += 1 #
-    print(f"Select your Choice : {choice}") #
-    match str(choice):
-        case "1":
-            num = 5
-            print(f"Enter your number of subject : {num}")
-            print("Press any key to continue . . .\n")
-        case "2":
-            print("Start Random  Score&Grade . . . .")
-            print("=" * 26)
-            for i in range(num):
-                point = randint(40,90)
-                check_grade = {80:"A",70:"B",60:"C",50:"D",0:"F"}
-                for c in check_grade:
-                    if point >= c:
-                        grade = check_grade[c]
-                        break
-                check_point = {"A":4,"B":3,"C":2,"D":1,"F":0}
-                for c in check_point:
-                    if grade == c:
-                        point = check_point[c] * 3
-                total_p += point
-                total_c += 3
-
-                print(f"Your #{i} subject is {grade}")
-            print(f"\nYour Total GPA is : {total_p / total_c}")
-            print("=" * 26)
-            print("Press any key to continue . . .\n")
-        case "3":
-            print("Exit Program . . .")
-            break
+line,head = "=" * 26,format("Main Menu","^26")
+print(line,head,line," 1. Input number of subject (8)"," 2. start Random Grade"," 3. Exit Program",line,sep="\n")
+choice = int(input("Select your Choice : "))
+match choice:
+    case 1:
+        num = int(input("Enter your number of subject : "))
+        print("Press any key to continue . . .")
+        for i in range(num):
+            score = randint(0,100)
+            if 80 <= score <= 100: grade,point = "A",4.0
+            elif 70 >= score: grade,point = "B",3.0
+            elif 60 >= score: grade,point = "C",2.0
+            elif 50 >= score: grade,point = "D",1.0
+            else: grade,point = "F",0
--- Midterm-67/number_palindrome.py
+++ Midterm-67/number_palindrome.py
@@ -22,17 +22,6 @@
 # for i in range(len(num)):
 #     print(" " * i,num[::-1][i:],num[1:(len(num)-i)],sep="")
 
-num,re = input("Enter number : "),""
-re += str(int(num) % 10 // 1)
-re += str(int(num) % 100 // 10)
-re += str(int(num) % 1000 // 100)
-re += str(int(num) % 10000 // 1000)
-re += str(int(num) % 100000 // 10000)
-re += str(int(num) % 1000000 // 100000)
-print(re)
-
-# for i in range(1,len(num)+1):
-#     re += str(int(num) % (10*i) // (1*i))
-# print(re)
-
+num = input("Enter number : ")
+for i in range(len(num)): print(" " * i,num[::-1][i:],num[1:(len(num) - i)],sep="")
 
