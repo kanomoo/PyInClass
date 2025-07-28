1f86128c4536d35a060393579915ff4b3d331a67
kanomoo <few717254@gmail.com>
2025-07-28 23:36:00 +0700

    test

--- Midterm-66/Pyramid.py
+++ Midterm-66/Pyramid.py
@@ -22,7 +22,15 @@
 
 #show in terminal
 # num = input("Enter string for pyramid : ")
-num = "RukSomchai"
-num = " ".join(num)
-for i in range(0,len(num),2):
-    print(" " * ((len(num) // 2) - (i // 2)),num[:i+1],sep="")
+# num = "RukSomchai"
+# num = " ".join(num)
+# for i in range(0,len(num),2):
+#     print(" " * ((len(num) // 2) - (i // 2)),num[:i+1],sep="")
+
+
+name,result = input("Enter string for Pyramid : "), ""
+for i in name:
+    result += i
+    result += " "
+for i in range(2,len(result)+1,2):
+    print(" " * (len(name) - i // 2),result[:i],sep="")
