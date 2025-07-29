2f3f62fdc0f15480337ed1393591c24466ea5e3a
Your Name <you@example.com>
2025-07-29 16:50:28 +0700

    test

--- AllChapter/chapter 5/Assignment2/Palindrome.py
+++ AllChapter/chapter 5/Assignment2/Palindrome.py
@@ -1,5 +1,5 @@
 print(">> Program Palindrome Number <<")
 num = input("Enter integer number : ")
 for i in range(len(num) // 2):
-    result = "Palindrome" if num[i] == num[len(num)-1 - i] else "Not Palindrome"
+    result = "Palindrome" if num[i] == num[len(num)-1 - i] else result = "Not Palindrome"
 print(result)
