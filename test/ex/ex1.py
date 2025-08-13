7f91a02a3edc32c66e487f3346a77b43f95e87dc
Few2 <few717254@gmail.com>
2025-08-13 08:23:10 +0700

    ex3

--- test/ex/ex1.py
+++ test/ex/ex1.py
@@ -1,4 +1,4 @@
-head = "| Main  Menu |"
+head = "| Main Menu |"
 line = "=" * len(head)
 while True:
     print(line,head,line," 1. Triangle 1\n 2. Triangle 2\n 3. Triangle 3\n 4. Triangle 4\n 5. Triangle 5\n 6. Exit",sep="\n")
@@ -8,38 +8,36 @@ while True:
             name = input("\nEnter name : ")
             print()
             for i in range(len(name)):
-                print(" " * i,name[i:],name[::-1][1:len(name) - i],sep="")
+                print(" " * i,name[i:],name[1:len(name)-i],sep="")
         case 2:
             name = input("\nEnter name : ")
             print()
             for i in range(len(name)):
-                print(" " * (len(name) - i-1), name[len(name) - i-1:], name[::-1][1:i+1], sep="")
+                print(" " * (len(name) - i-1),name[len(name) - i-1:],name[::-1][1:i+1],sep="")
         case 3:
-            Name, name = input("\nEnter name : "), ""
-            for i in Name:
-                name += i
-                name += " "
+            Name, name = "", input("\nEnter name : ")
             print()
-            for i in range(0,len(name),2):
-                print(" " * ((len(Name) - (i//2))-1),name[:i+1],sep="")
+            for i in name:
+                Name += i
+                Name += " "
+            for i in range(0,len(Name),2):
+                print(" " * (len(name) - (i//2)-1),Name[:i+1],sep="")
         case 4:
-            Name,name = input("\nEnter name : "), ""
-            for i in Name:
-                name += i
-                name += " "
+            Name, name = "", input("\nEnter name : ")
             print()
-            for i in range(0,len(name),2):
-                print(" " * (i//2),name[:len(name) - i],sep="")
+            for i in name:
+                Name += i
+                Name += " "
+            for i in range(0, len(Name), 2):
+                print(" " * (i//2 ),Name[:len(Name) - i],sep="")
         case 5:
             name = input("\nEnter name : ")
             print()
             for i in range(len(name)):
                 print(" " * (len(name) - i - 1), name[len(name) - i - 1:], name[::-1][1:i + 1], sep="")
             for i in range(len(name)):
-                print(" " * i,name[i:],name[::-1][1:len(name) - i],sep="")
+                print(" " * i,name[i:],name[::-1][1:len(name)-i],sep="")
         case 6:
             print("Exit Program...")
             break
-        case _:
-            print("No choice...")
     print()
--- test/ex/ex2.py
+++ test/ex/ex2.py
@@ -1,67 +1,21 @@
-head2 = ">> Program Calculate Grade <<"
-line2, result, tc, tp = "=" * len(head2),"", 0 , 0
-print(line2,head2,line2,sep="\n")
-print("\nInput Data:")
+head = ">> Program Calculate Grade <<"
+line, result, tc, tp = "=" * len(head), "", 0, 0
+print(head,line,"\nInput Data:",sep="\n")
 for i in range(6):
-    # name = "A"
-    # score = 76
-    name = input(f"Enter subject name({i+1}) : ")
-    score = float(input(f"Enter subject score({i+1}) : "))
+    name = input("Enter subject name : ")
+    score = int(input("Enter subject score : "))
     print()
-    if score >= 80 and score <= 100: grade,point = "A",4 * 3
-    elif score >= 70 and score <= 79: grade,point = "B", 3 * 3
-    elif score >= 60 and score <= 69: grade, point = "C", 2 * 3
-    elif score >= 50 and score <= 59: grade,point = "D", 1 * 3
-    elif score >= 0 and score <= 49: grade, point = "F", 0 * 3
+    if score >= 80 and score <= 100 : grade, point = "A", 4 * 3
+    elif score >= 70 and score <80 : grade, point = "B" , 3 * 3
+    elif score >= 60 and score <70 : grade, point = "C" , 2 * 3
+    elif score >= 50 and score <60 : grade, point = "D" , 1 * 3
+    elif score >=  0 and score <50 : grade, point = "F" , 0 * 3
+    result += f":   {i+1}   : {name:<{len("Subject Name                 ")}}: {score:4.1f} :   {grade}   :   {3}   : {point:4.1f} :\n"
     tc += 3
     tp += point
-    result += f":   {i+1}   :{name:<{len(" Subject Name               ")}}: {score:4.1f} :   {grade}   :   {3}   : {point:4.1f} :\n"
-head = ":Sub No.: Subject Name               : Mark : Grade :Credits:Points:"
-line = "=" * len(head)
-result += line
-print(format("Grade Report",f">{len(":Sub No.: Subject Name  "+"Grade Report")}"),line,head,line,result,sep="\n")
-print(f":{"Total":>{len("Sub No.: Subject Name               :")}}              :  {tc}   : {tp:4.1f} :",line,sep="\n")
-print(f": {f"Grade Point Average (GPA) : {tp / tc:4.2f}":<{len("ub No.: Subject Name               : Mark : Grade :Credits:Points")}}:",line,sep="\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# salary = 0
-# income = 0
-# discount = 100000
-# net_income = 0
-# while True:
-#     print("=" * 17)
-#     print("| Tax Main Menu |")
-#     print("=" * 17)
-#     print(" 0. Exit ")
-#     print(" 1. Input Salary","({:,.2f})".format(salary),sep="")
-#     print(" 2. Display Tax")
-#     choice = input("Enter choice : ")
-#     if choice == "0":
-#         break
-#     elif choice == "1":
-#         salary = int(input("Enter salary : "))
-#     elif choice == "2":
-#         income = salary * 12
-#         net_income = income - discount
-#         print("Salary :","({:,.2f})".format(salary))
-#         print("Income :","({:,.2f})".format(income))
-#         print("Discount :","({:,.2f})".format(discount))
-#         print("Net Income :","({:,.2f})".format(net_income))
-#         print()
-#         print("Report Tax:")
-#         print("=" * 60)
-#         print(f"|{"Net  Income":^17}|Tax Rate|{"Tax":^20}|")
-#         if salary <= 150000:
-#             print(f"|{"1-150,000.00":>17}|{"0%":>5}|{f"{salary}" "* 0.00":<10}|{(salary * 0.00):>10,}|")
+h = ":Sub No.: Subject Name                 : Mark : Grade :Credits:Points:"
+l = "=" * len(h)
+result += l
+print(f"{"Grade Report":>{len(":Sub No.: Subject Name                 :")}}",l,h,l,result,sep="\n")
+print(f":{"Total":^{len("Sub No.: Subject Name                 : Mark : Grade ")}}:  {tc:2}   : {tp:4.1f} : ",l,sep="\n")
+print(f": {f"Grade Point Average (GPA)  : {tp / tc:.2f}":<{len("ub No.: Subject Name                 : Mark : Grade :Credits:Points")}}:",l,sep="\n")
