2bcc8cc895aa823e7adaa886cd2da1e2c723cef5
kanomoo <few717254@gmail.com>
2025-08-31 15:29:21 +0700

    c7

---
+++ AllChapter/chapter 7/test.py
@@ -0,0 +1,34 @@
+def add (a, b):
+    return(a+b)
+
+print(add(20,30))
+
+add2 = lambda a,b: a+b
+print(add(20,30))
+
+# def show_local():
+#     global x
+#     y = 10
+#     x = x + y
+#     print("x = ", x)
+#     print("y = ", y)
+#
+# x = 5
+# show_local()
+# print(x)
+
+
+# def add_value():
+#     return (20+30)
+# print(add_value())
+
+
+# def say_message(text):
+#     print(text)
+# say_message("Hello World")
+
+
+# def say_hello():
+#     print("Hello")
+#
+# say_hello()
